f550ac8da42b527c9c95bf7d8daf87516601e765
happy365lyzh <503887183@qq.com>
2016-01-31 17:06:24 +0800

    999-1

--- index.js
+++ index.js
@@ -18,3 +18,6 @@ console.log("ddd");
 console.log("ddd");
 
 console.log("ddd");
+
+console.log("ddd");
+console.log("ddd");
---
+++ index2.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ThinkPad on 2016/1/31.
+ */
+(function(w,u){
+
+})(window,undefined);
