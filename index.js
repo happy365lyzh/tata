a66e220885443720639ce0c671e733ae69d58cec
happy365lyzh <503887183@qq.com>
2016-01-31 16:44:20 +0800

    secend
    secend

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  */
 var tootls = (function(){
     var too = {aaa:'110'};
+    var too = {aaa:'110',bbb:'111'};
     return too;
 }());
 
-console.log(tootls.aaa)
+console.log(tootls.aaa);
+
+
+console.log(tootls.bbb);
