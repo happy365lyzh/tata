da079f71df360536cf0bc304855c2e82b3168d5a
happy365lyzh <503887183@qq.com>
2016-01-31 17:00:08 +0800

    11
    11

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
  * Created by ThinkPad on 2016/1/31.
  */
 var tootls = (function(){
-    var too = {aaa:'110'};
     var too = {aaa:'110',bbb:'111'};
     return too;
 }());
 
 console.log(tootls.aaa);
 
+console.log("ddd");
 
 console.log(tootls.bbb);
+
+
+console.log("ddd");
+
+console.log("ddd");
+
+console.log("ddd");
